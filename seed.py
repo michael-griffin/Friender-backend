208c519ffa24e22dc00edd33e57ffbde780894ee
Michael <michaellngriffin@gmail.com>
2023-10-13 15:31:23 -0400

    expanded seed file

--- seed.py
+++ seed.py
@@ -7,41 +7,72 @@ db.create_all()
 
 user1 = User.signup(username='test_user1',
                     password='password',
-                    hobbies='not much',
-                    interests='testing',
+                    hobbies='skateboarding, cooking',
+                    interests='coding at rithm',
                     location=10000,
-                    radius=2)
+                    radius=50)
+
+
 user2 = User.signup(username='test_user2',
                     password='password',
-                    hobbies='not much',
+                    hobbies='taking vitamins',
                     interests='testing',
-                    location=10001,
+                    location=10002,
                     radius=2)
+
 user3 = User.signup(username='test_user3',
                     password='password',
-                    hobbies='not much',
+                    hobbies='base jumping',
                     interests='testing',
                     location=10003,
                     radius=2)
 
 user4 = User.signup(username='test_user4',
                     password='password',
-                    hobbies='not much',
+                    hobbies='mountaineering',
                     interests='testing',
-                    location=10002,
+                    location=10004,
                     radius=2)
 
 user5 = User.signup(username='test_user5',
                     password='password',
-                    hobbies='not much',
+                    hobbies='origami',
                     interests='testing',
-                    location=10002,
+                    location=10005,
                     radius=50)
 
 user6 = User.signup(username='test_user6',
                     password='password',
-                    hobbies='living away from people',
+                    hobbies='calligraphy',
                     interests='testing',
+                    location=10006,
+                    radius=50)
+
+user7 = User.signup(username='test_user7',
+                    password='password',
+                    hobbies='juggling',
+                    interests='testing',
+                    location=10007,
+                    radius=50)
+
+user8 = User.signup(username='test_user8',
+                    password='password',
+                    hobbies='puppetry, soap making',
+                    interests='testing',
+                    location=10008,
+                    radius=50)
+
+user9 = User.signup(username='test_user9',
+                    password='password',
+                    hobbies='woodworking',
+                    interests='testing',
+                    location=10012,
+                    radius=50)
+
+user10 = User.signup(username='test_user6',
+                    password='password',
+                    hobbies='living away from people',
+                    interests='being alone',
                     location=60002,
                     radius=2)
 
@@ -54,9 +85,18 @@ image3 = Image.add_image('test_user3', 'mem-game2.png')
 
 rating1 = Rating.add_rating('test_user1', 'test_user2', True)
 rating2 = Rating.add_rating('test_user2', 'test_user1', True)
-rating3 = Rating.add_rating('test_user1', 'test_user3', True)
+rating3 = Rating.add_rating('test_user3', 'test_user1', True)
+rating4 = Rating.add_rating('test_user4', 'test_user1', True)
+rating5 = Rating.add_rating('test_user5', 'test_user1', True)
+rating6 = Rating.add_rating('test_user6', 'test_user1', True)
+rating7 = Rating.add_rating('test_user7', 'test_user1', True)
+rating8 = Rating.add_rating('test_user8', 'test_user1', True)
+rating9 = Rating.add_rating('test_user9', 'test_user1', True)
+rating10 = Rating.add_rating('test_user10', 'test_user1', True)
 
-message1 = Message.add_message('test_user1', 'test_user2', 'first')
+
+
+message1 = Message.add_message('test_user1', 'test_user2', 'first post')
 message2 = Message.add_message('test_user2', 'test_user1', 'second')
 message3 = Message.add_message(
     'test_user1', 'test_user2', 'how are you doing today?')
